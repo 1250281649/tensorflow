89db64485fff5794b1a1bb2ebf8f1e8ead062419
xiong.guo <1250281649@qq.com>
2020-01-18 15:24:06 +0800

    [NEW] init code base

---
+++ tensorflow/lite/project/compile/build_arm64.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd $(dirname $0)
+
+CXX=aarch64-linux-gnu-g++
+CC=aarch64-linux-gnu-gcc
+
+build_dir=../../../out/build_arm64
+cd ${build_dir}
+cmake ../../ -DCMAKE_CXX_COMPILER=aarch64-linux-gnu-g++ -DCMAKE_C_COMPILER=aarch64-linux-gnu-gcc
+make -j4
