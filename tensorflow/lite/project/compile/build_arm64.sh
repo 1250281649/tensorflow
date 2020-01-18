a4a35517aa0c8c313d3749e94836f1dcdfb4db1a
xiong.guo <1250281649@qq.com>
2020-01-18 17:32:49 +0800

    [NEW] add mobilenet test data and build_x64.sh

--- tensorflow/lite/project/compile/build_arm64.sh
+++ tensorflow/lite/project/compile/build_arm64.sh
@@ -4,7 +4,8 @@ cd $(dirname $0)
 CXX=aarch64-linux-gnu-g++
 CC=aarch64-linux-gnu-gcc
 
-build_dir=../../../out/build_arm64
+build_dir=../../out/build_arm64
+mkdir -p ${build_dir}
 cd ${build_dir}
 cmake ../../ -DCMAKE_CXX_COMPILER=aarch64-linux-gnu-g++ -DCMAKE_C_COMPILER=aarch64-linux-gnu-gcc
 make -j4
---
+++ tensorflow/lite/project/compile/build_x64.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd $(dirname $0)
+
+build_dir=../../out/build
+mkdir -p ${build_dir}
+cd ${build_dir}
+cmake ../../
+make -j4
