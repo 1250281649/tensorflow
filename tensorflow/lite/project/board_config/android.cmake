7e7bb620ddd58d9754e4c74d49d683a7ee86d997
xiong.guo <1250281649@qq.com>
2020-01-21 00:24:25 +0800

    [BUG] add gpu delegate and benchmarklib on android

--- tensorflow/lite/project/board_config/android.cmake
+++ tensorflow/lite/project/board_config/android.cmake
@@ -1,14 +1,12 @@
-#Linux 64 bit configurations
-
-set(BOARD_TARGET Linux_x64)
-
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -g -fPIC -pthread -std=c++11 -Wno-ignored-attributes")
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -g -fPIC")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O3 -fPIC -pthread -std=c++11 -Wno-ignored-attributes")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -O3 -fPIC")
 set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -ldl")
 
 add_definitions(-D CL_TARGET_OPENCL_VERSION=220)
+add_definitions(-D EGL_EGLEXT_PROTOTYPES)
 add_definitions(-D GEMMLOWP_ALLOW_SLOW_SCALAR_FALLBACK)
 add_definitions(-D ABSL_NO_XRAY_ATTRIBUTES=1)
 add_definitions(-D BUILD_TESTING=OFF)
 
-set(BUILD_GPU_DELEGATE  off)
+set(BUILD_GPU_DELEGATE  on)
+set(BUILD_BENCHMARK on)
