82d78e3a02772229661abd81650e40f5c601c119
xiong.guo <1250281649@qq.com>
2020-02-01 22:52:37 +0800

    [NEW] add android v7 script

---
+++ tensorflow/lite/project/compile/build_android_v7.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+cd $(dirname $0)
+
+build_dir=../../out/build_android_v7
+ndk_dir=/opt/android/android-ndk-r20b
+
+mkdir -p ${build_dir}
+cd ${build_dir}
+cmake ../../ \
+    -DCMAKE_ANDROID_NDK=${ndk_dir} \
+    -DCMAKE_TOOLCHAIN_FILE=${ndk_dir}/build/cmake/android.toolchain.cmake \
+    -DANDROID_TOOLCHAIN=clang \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DANDROID_ABI=armeabi-v7a \
+    -DANDROID_PLATFORM=18 \
+    -DCMAKE_CXX_STANDARD=11 \
+    -DBOARD_TARGET=Android 
+
+make -j3
